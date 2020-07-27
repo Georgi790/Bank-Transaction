ecc7ebd7cf60ef24eb383a4845ebd4d3f6855aa6
shevartv <shevartov@gmail.com>
2020-07-27 10:28:18 +0300

    Initial commit

---
+++ Transaction/Migrations/20200714061947_FloatAmount.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transaction.Migrations
+{
+    public partial class FloatAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(10)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "Transactions",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AcountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BankName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AcountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
